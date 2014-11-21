cee14db817352d9797fb487f86e7272967d617ea
dev <dev@lacourarie.org>
2014-11-21 18:56:10 +0100

    Commit initial.

---
+++ msp430-template/includes/%{APPNAMELC}.h
@@ -0,0 +1,22 @@
+/**
+ * <LICENCE>
+ *
+ * 
+ */
+ #ifndef %{APPNAMELC}
+ #define %{APPNAMELC}
+ 
+ void setup();
+ 
+ void loop();
+ 
+ #endif // %{APPNAMELC}
+
+
+
+
+
+
+
+
+
---
+++ msp430-template/main.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "%{APPNAMELC}.h"
+
+
+
+int main() {
+    
+    setup();
+    
+    while(true)
+    {
+        loop();
+    }
+    
+};
+
+
